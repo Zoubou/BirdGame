2d5b1c0bfc76b4bea9f571fd4ee1ac7bfc6c5e73
zoubou <your.jzoub03@gmail.com>
2025-12-07 21:11:53 +0200

    Initial commit

---
+++ Assets/PipeMoveScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour
+{
+    public float moveSpeed = 20;
+    public float deadZone = -80;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if(transform.position.x < deadZone)
+        {
+            Destroy(gameObject);
+        } 
+    }
+}
